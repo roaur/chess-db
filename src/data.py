ed09227d532badbedb3802bcf76f29670dfe0da5
Roman Auriti <roman@somewhatspatial.com>
2024-05-16 06:51:16 -0600

    split work

---
+++ src/data.py
@@ -0,0 +1,108 @@
+import asyncio
+from db import get_db_connection
+import logging
+import requests
+import json
+from utils import handle_player_data
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s %(levelname)s %(name)s: %(message)s"
+)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class DataFetcher:
+    def __init__(self, url):
+        self.url = url
+
+    def fetch_data(self):
+        while True:
+            try:
+                response = requests.get(self.url, stream=True)
+                for line in response.iter_lines():
+                    if line:  # filter out keep-alive new lines
+                        decoded_line = line.decode('utf-8')
+                        data = json.loads(decoded_line)
+                        logging.debug(f"Received data: {data}")
+                        yield data
+            except requests.ChunkedEncodingError:
+                logging.error("Error fetching data. Retrying...")
+                continue
+            except Exception as e:
+                logging.error(f"Error fetching data: {e}")
+                break
+
+
+class DataProcessor:
+    def __init__(self):
+        self.game_id = None
+        self.move_num = 0
+
+    def generate_move_id(self):
+        return f"{self.game_id}-{self.move_num}"
+
+    async def handle_data(self, data):
+        if data['t'] == 'featured':
+            # Insert new game and players into database
+            self.game_id = data['d']['id']
+            await self.insert_players(data['d']['players'])
+            await self.insert_game(self.game_id, data['d'])
+        elif data['t'] == 'fen' and self.game_id is not None:
+            self.move_num += 1
+            # Insert move into database
+            await self.insert_move(data['d'], self.game_id)
+
+    async def insert_game(self, game_id, game_data):
+        # Insert game into database
+        player_data = await handle_player_data(game_data)
+        id = game_id
+        game_orientation = game_data['orientation']
+        white_player_id = player_data['white']['id']
+        black_player_id = player_data['black']['id']
+        white_rating = player_data['white']['rating']
+        black_rating = player_data['black']['rating']
+        initial_fen = game_data['fen']
+        async with get_db_connection() as aconn:
+            async with aconn.cursor() as acur:
+                logger.debug(f"Inserting game {id} into database.")
+                await acur.execute(
+                    "INSERT INTO games (game_id, white_player_id, black_player_id, white_rating, black_rating, initial_fen, orientation) VALUES (%s, %s, %s, %s, %s, %s, %s) ON conflict do nothing;",
+                    (id, white_player_id, black_player_id, white_rating, black_rating, initial_fen, game_orientation)
+                )
+                await aconn.commit()
+                logger.debug(f"Game {id} inserted into database.")
+
+
+    async def insert_players(self, players):
+        for player in players:
+            # Insert player into database
+            user = player['user']
+            player_name = user['name']
+            player_title = user['title'] if 'title' in user else None
+            player_id = user['id']
+            async with get_db_connection() as aconn:
+                async with aconn.cursor() as acur:
+                    logger.debug(f"Upserting player {player_name} into database.")
+                    await acur.execute(
+                        "INSERT INTO players (player_id, name, title) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING;",
+                        (player_id, player_name, player_title)
+                    )
+                    await aconn.commit()
+                    logger.debug(f"Player {player_name} upserted into database.")
+
+
+    async def insert_move(self, move, game_id):
+        fen = move['fen']
+        last_move = move['lm']
+        white_clock = move['wc']
+        black_clock = move['bc']
+        async with get_db_connection() as aconn:
+            async with aconn.cursor() as acur:
+                logger.debug(f"Inserting move {last_move} into database.")
+                await acur.execute(
+                    "INSERT INTO moves (move_id, game_id, fen, last_move, white_clock, black_clock) VALUES (%s, %s, %s, %s, %s, %s);",
+                    (self.generate_move_id(), game_id, fen, last_move, white_clock, black_clock)
+                )
+                await aconn.commit()
+                logger.debug(f"Move {last_move} inserted into database.")
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import json
 import logging
 import asyncio
 from stream_context import StreamContext
+from data import DataFetcher, DataProcessor
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -11,23 +12,32 @@ logger.setLevel(logging.DEBUG)
 
 
 def fetch_data(url):
-    response = requests.get(url, stream=True)
-    for line in response.iter_lines():
-        if line:  # filter out keep-alive new lines
-            decoded_line = line.decode('utf-8')
-            data = json.loads(decoded_line)
-            logger.debug(f"Received data: {data}")
-            yield data
+    while True:
+        try:
+            response = requests.get(url, stream=True)
+            for line in response.iter_lines():
+                if line:  # filter out keep-alive new lines
+                    decoded_line = line.decode('utf-8')
+                    data = json.loads(decoded_line)
+                    logger.debug(f"Received data: {data}")
+                    yield data
+        except requests.ChunkedEncodingError:
+            logger.error("Error fetching data. Retrying...")
+            continue
+        except Exception as e:
+            logger.error(f"Error fetching data: {e}")
+            break
 
 
 async def stream_lichess_tv():
     url = "https://lichess.org/api/tv/feed"
-    data_stream = StreamContext()
+    data_fetcher = DataFetcher(url)
+    data_stream = StreamContext(data_fetcher, DataProcessor())
     with concurrent.futures.ThreadPoolExecutor() as executor:
         loop = asyncio.get_running_loop()
         data_generator = await loop.run_in_executor(executor, fetch_data, url)
         for data in data_generator:
-            await data_stream.handle_data(data)
+            await data_stream.processor.handle_data(data)
 
 
 # Run the streaming function
